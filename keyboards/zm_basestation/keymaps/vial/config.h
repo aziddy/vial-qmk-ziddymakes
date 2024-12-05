9fa20164cb9fa57f7c75046eeb424080b130bef5
Alex Zidros <alexander.zidros@gmail.com>
2024-12-05 16:13:28 -0500

    ZM Basestation - first (12k + Big Knob)

---
+++ keyboards/zm_basestation/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA3, 0xC4, 0x6B, 0x80, 0xA6, 0xBC, 0x1F, 0x98}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
---
+++ keyboards/zm_basestation/readme.md
@@ -0,0 +1,1 @@
+qmk compile -kb zm_basestation -km vial
