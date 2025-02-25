579a8594b75ce98e16df796b1a4cac86957c8a85
Alex Zidros <alexander.zidros@gmail.com>
2025-02-25 18:44:47 -0500

    ZM7K PE cant do both top and bot light right now ):

---
+++ keyboards/zm_7k_pe/readme.md
@@ -0,0 +1,30 @@
+# ZM-7K - PE
+
+## Outside known as ZM7K V3
+
+## Compile
+```bash
+qmk compile -kb zm_7k_pe -km vial
+```
+
+## Flash
+UF2 Bootloader / Drag and Drop
+
+## Leds
+
+### Under Led Pin: 
+WS_DATA / GP9   
+Qunatity: 4     
+RGBLIGHT
+
+
+### Switch Led Pin: 
+SK_DATA/ GP10   
+Qunatity: 7     
+RGBMATRIX
+
+
+## Clean QMK /.build Folder
+```
+qmk clean
+```
---
+++ keyboards/zm_7k_pe/rules.mk
@@ -0,0 +1,15 @@
+# # Add the custom driver
+# SRC += drivers/led/ws2812_custom.c
+
+# # Define the custom driver
+# RGB_MATRIX_CUSTOM_DRIVER = yes
+
+# # Enable RGB Matrix but disable the built-in WS2812 driver
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = custom
+
+# # Enable RGBlight
+# RGBLIGHT_ENABLE = yes
+
+# # Define the WS2812 driver for the matrix
+# WS2812_DRIVER = vendor
