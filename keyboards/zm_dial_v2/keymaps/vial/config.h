dd62ff066913a3aa9f23baa0f9d30d096c296654
Alex Zidros <alexander.zidros@gmail.com>
2025-09-17 14:22:42 -0400

    ZM DIal V2

---
+++ keyboards/zm_dial_v2/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0xC4, 0x6A, 0x80, 0xA6, 0xBC, 0x1F, 0x92}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
