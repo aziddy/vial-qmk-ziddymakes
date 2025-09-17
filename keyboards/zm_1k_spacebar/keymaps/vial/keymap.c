b8f916d6298236204cdc6e4315ca9c89af99d280
Alex Zidros <alexander.zidros@gmail.com>
2025-09-17 16:03:11 -0400

    ZM 1K Spacebare Vial Layout Improvements

--- keyboards/zm_1k_spacebar/keymaps/vial/keymap.c
+++ keyboards/zm_1k_spacebar/keymaps/vial/keymap.c
@@ -23,8 +23,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_zm_1k_spacebar(KC_SPC)};
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-};
-#endif
