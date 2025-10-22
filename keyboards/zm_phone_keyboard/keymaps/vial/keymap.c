cb7e99779185630ae4e80e9b5d3d9b38c7afc299
Alex Zidros <alexander.zidros@gmail.com>
2025-10-22 14:56:41 -0400

    working - zm_phone_keyboard

--- keyboards/zm_phone_keyboard/keymaps/vial/keymap.c
+++ keyboards/zm_phone_keyboard/keymaps/vial/keymap.c
@@ -34,12 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------'
      */
     [0] = LAYOUT_zm_phone_keyboard(
-        KC_Q,    KC_E,    KC_R,    KC_U,    KC_O,
-        KC_W,    KC_S,    KC_G,    KC_H,    KC_L,
-        KC_SLSH,    KC_D,    KC_T,    KC_Y,    KC_I,
-        KC_A,    KC_P,    KC_RBRC,    KC_V,    KC_BSPC,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-        KC_SPACE,    KC_Z,    KC_C,    KC_N,    KC_0,
-        KC_BSLS,    KC_LBRC,    KC_F,    KC_J,    KC_K
+        KC_Q,     KC_E,     KC_R,     KC_U,     KC_O,
+        KC_W,     KC_S,     KC_G,     KC_H,     KC_L,
+        KC_SLSH,  KC_D,     KC_T,     KC_Y,     KC_I,
+        KC_A,     KC_P,     KC_RBRC,  KC_1,     KC_BSPC,
+        KC_DOT,   KC_X,     KC_V,     KC_B,     KC_VOLU,
+        KC_SPACE, KC_Z,     KC_C,     KC_N,     KC_M,
+        KC_BSLS,  KC_LBRC,  KC_F,     KC_J,     KC_K
     )
-};
--- keyboards/zm_phone_keyboard/zm_phone_keyboard_pinout.md
+++ keyboards/zm_phone_keyboard/zm_phone_keyboard_pinout.md
@@ -14,3 +14,31 @@
 | COL3       | GPIO12             |
 | COL4       | GPIO1              |
 | COL5       | GPIO3              |
+
+
+
+## Physical Key Pinout
+
+
+ROW1 = PIN6 Q, E, R, U, O
+ROW2 = PIN8 W, S, G, H, L
+ROW3 = PIN18 T, Y, I, D, sym
+ROW4 = PIN20 P, A, bck, ret, sh2
+ROW5 = PIN21 alt, X, V, $, B
+ROW6 = PIN22 spce, Z, C, M, N
+ROW7 = PIN27 mike, sh1, F, K, J
+
+Keyboard “column” mappings (inner circle)
+
+COL1 = PIN7 Q, W, A, sym, alt, mike, space
+COL2 = PIN9 S, D, X, sh1, E, P, Z
+COL3 = PIN10 R, T, F, G, V, C, sh2
+COL4 = PIN17 Y, U, ret, H, B, J, N
+COL5 = PIN19 I, O, K, L, bck, M, $
+
+
+## Physical Key Layout
+| Q    | W    | E    | R    | T    | Y    | U    | I    | O    |  P    |
+| A    | S    | D    | F    | G    | H    | J    | K    | L    | BSPC  |
+| DOT  | Z    | X    | C    | V    | B    | N    | M    | VOLU |  1    |
+| LBRC | BSLS | SPACE| SLSH | RBRC | 
