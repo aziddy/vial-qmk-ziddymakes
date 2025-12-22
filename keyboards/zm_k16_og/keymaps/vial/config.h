c36e7cbf3524aa120ea0878c0863da876fa3faae
Alex Zidros <alexander.zidros@gmail.com>
2025-12-22 12:56:46 -0500

    ZMK16_OG

---
+++ keyboards/zm_k16_og/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0xD4, 0x7B, 0x91, 0xB7, 0xCD, 0x2F, 0xA1}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+
